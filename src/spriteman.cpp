dcf3d1fcf0bd821224f58a0fe1803f476b145d33
thatlittlegit <wapidstyle@live.ca>
2017-09-10 15:17:30 -0400

    Add a class for sprites

--- src/spriteman.cpp
+++ src/spriteman.cpp
@@ -4,7 +4,53 @@
 #include <vector>
 #include <iostream>
 
-std::vector<sf::Sprite> fetchSprites(std::string spriteFolder) {
+using std::string;
+
+namespace SuperTTD {
+	class Sprite {
+	private:
+		string filename;
+	  int world;
+	  string id;
+		sf::Sprite associated;
+
+		void construct(string argFilename, unsigned int argWorld, string argId) {
+			filename = argFilename;
+			world = argWorld;
+			id = argId;
+
+			try {
+				reloadSprite();
+			} catch (const std::invalid_argument& e) {}
+		}
+
+	public:
+		string getFilename() { return filename; }
+		int getWorld() { return world; }
+		string getId() { return id; }
+
+		sf::Sprite reloadSprite() {
+			sf::Texture texture;
+
+			if(!texture.loadFromFile("sprites/" + filename)) {
+				throw std::invalid_argument("Unable to load sprite");
+			} else {
+				associated.setTexture(texture);
+				return associated;
+			}
+		}
+
+		Sprite(string argFilename, int argWorld, string argId) {
+			construct(argFilename, argWorld, argId);
+		}
+
+		Sprite(YAML::Node yaml) {
+			construct(yaml["filename"].as<string>(), yaml["world"].as<unsigned int>(), yaml["id"].as<string>());
+		}
+	};
+}
+
+std::vector<sf::Sprite> fetchSprites(string spriteFolder) {
 	YAML::Node sprites = YAML::LoadFile(spriteFolder + "/sprites.yml")["sprites"];
 
   for(unsigned int index = 0; index < sprites.size(); index++) {
