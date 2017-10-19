1fda0b6dc8306f2484cd93ef939987d38f9a2470
thatlittlegit <wapidstyle@live.ca>
2017-10-19 19:45:33 -0400

    Add license header

--- include/spriteman.hpp
+++ include/spriteman.hpp
@@ -1,3 +1,16 @@
+/* (c) 2017 thatlittlegit and PinguPenguin.
+ * This file is from the SuperTTD project.
+ *
+ * SuperTTD, and by extension this file, is free software under
+ * the GNU General Public License, 2.0.
+ *
+ * SuperTTD is distributed in the hope that it will be useful,
+ * but comes with NO WARRANTY WHATSOEVER. Please se the official
+ * GNU GPL 2.0 for more information.
+ *
+ * You should have recieved a copy of the GPL with this project.
+ * If not, see https://gnu.org/licenses/.
+ */
 #include <SFML/Graphics.hpp>
 #include <yaml-cpp/yaml.h>
 #include <string>
