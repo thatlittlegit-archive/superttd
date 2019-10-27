37c1a2bee240f0158e3c6801fc5e4d4d865c4190
thatlittlegit <personal@thatlittlegit.tk>
2019-10-27 17:43:27 -0400

    Update copyright notices to remove typo and update formatting

--- src/namespaced_id.cpp
+++ src/namespaced_id.cpp
@@ -1,15 +1,13 @@
-/* (c) 2019 thatlittlegit and PinguPenguin.
- * This file is from the SuperTTD project.
+/* (c) 2019 thatlittlegit & PinguPenguin. This file's from the SuperTTD project.
  *
- * SuperTTD, and by extension this file, is free software under
- * the GNU General Public License, 2.0.
+ * SuperTTD, and by extension this file, is free software under the GNU General
+ * Public License, 2.0.
  *
- * SuperTTD is distributed in the hope that it will be useful,
- * but comes with NO WARRANTY WHATSOEVER. Please se the official
- * GNU GPL 2.0 for more information.
+ * SuperTTD is distributed in the hope that it will be useful, but comes with NO
+ * WARRANTY WHATSOEVER. Please see the GNU GPL 2.0 license for more information.
  *
- * You should have recieved a copy of the GPL with this project.
- * If not, see https://gnu.org/licenses/.
+ * You should have recieved a copy of the GPL with this project. If not, see
+ * https://gnu.org/licenses/.
  */
 #include "namespaced_id.hpp"
 #include <algorithm>
--- src/superttd.hpp
+++ src/superttd.hpp
@@ -1,15 +1,14 @@
-/* (c) 2017 thatlittlegit and PinguPenguin.
- * This file is from the SuperTTD project.
+/* (c) 2017 thatlittlegit & PinguPenguin. This file's from the SuperTTD
+ * project.
  *
- * SuperTTD, and by extension this file, is free software under
- * the GNU General Public License, 2.0.
+ * SuperTTD, and by extension this file, is free software under the GNU General
+ * Public License, 2.0.
  *
- * SuperTTD is distributed in the hope that it will be useful,
- * but comes with NO WARRANTY WHATSOEVER. Please se the official
- * GNU GPL 2.0 for more information.
+ * SuperTTD is distributed in the hope that it will be useful, but comes with NO
+ * WARRANTY WHATSOEVER. Please see the GNU GPL 2.0 license for more information.
  *
- * You should have recieved a copy of the GPL with this project.
- * If not, see https://gnu.org/licenses/.
+ * You should have recieved a copy of the GPL with this project. If not, see
+ * https://gnu.org/licenses/.
  */
 #pragma once
 
